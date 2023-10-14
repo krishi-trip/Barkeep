d0494cb99d36d86bc03f79007b896951f1729df8
krishi-trip <66849513+krishi-trip@users.noreply.github.com>
2023-10-14 16:15:43 -0400

    Added some simple booleans and filter

---
+++ webScraper.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+
+#URL of the FicoForums website
+BASE_URL = 'https://ficoforums.myfico.com/'
+
+#URL of the credit card forums
+FORUM_URL = 'https://ficoforums.myfico.com/t5/Credit-Cards/bd-p/creditcard'
+
+# A common user agent, can help bypass some scraping restrictions
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+#Parameters
+min_kudos = 10
+debug_mode = 0
+
+
+def get_post_links_and_titles(url):
+  #List that has all the post details
+  post_list = []
+
+  #For every page in the credit card forums (1, 4384)
+  for i in range(1, 2):
+    #If it is the first page, keep the URL as is
+    if i == 1:
+      response = requests.get(url, headers=headers)
+
+    #If if is any other page, append the page number to the URL
+    else:
+      new_URL = "{}/page/{}".format(url, i)
+      response = requests.get(new_URL, headers=headers)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #Finds all of the posts on the page
+    posts = soup.find_all('h2', class_='message-subject')
+
+    for post in posts:
+      #Gets the link of the post
+      link_tag = post.find('a', class_='page-link')
+      title = link_tag.text.strip()
+      link = BASE_URL + link_tag['href']
+      post_list.append({
+          'title': title,
+          'link': link
+      })
+
+  return post_list
+
+post_list = get_post_links_and_titles(FORUM_URL)
+if debug_mode == 1:
+  print(post_list[0]['link'])
+
+def get_post_data_and_kudos(post_list):
+  post_details = []
+
+  for post in post_list:
+    url = post['link']
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    details = soup.find_all('div', class_='lia-message-body-content')
+    kudos = soup.find_all('span', class_='MessageKudosCount')
+
+    for i in range(len(details)):
+      post_kudos = int(kudos[i].text.strip())
+      if post_kudos < min_kudos:
+        break
+      post_text = details[i].text.strip()
+      post_details.append({
+          'Message': post_text,
+          'Kudos': post_kudos
+      })
+
+    return post_details
+
+post_details = get_post_data_and_kudos(post_list)
+if debug_mode == 1:
+  for post in post_details:
+    print(f"Message: {post['Message']}\nKudos: {post['Kudos']}\n{'-'*40}")
