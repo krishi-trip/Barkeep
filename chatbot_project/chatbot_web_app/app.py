c610bdc7fb2323bc40d106f8e61af45c1257f43e
Rohan Manne <rmanne812@gmail.com>
2023-12-03 16:20:18 -0500

    trying to add ChatGPT to parse user responses

--- chatbot_project/chatbot_web_app/app.py
+++ chatbot_project/chatbot_web_app/app.py
@@ -1,44 +1,75 @@
 from flask import Flask, render_template, request, jsonify
+from openai import OpenAI
 
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-
+client = OpenAI(api_key='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')
 
 app = Flask(__name__)
+
+questions = [
+    "Are you interested in earning rewards for travel-related expenses like flights and hotels? If so, what percentage of cash back would you find appealing for these expenses?",
+    "Would you like to earn cash back for entertainment expenses such as movie theaters, amusement parks, and tourist attractions? Please specify the cash back percentage you're looking for.",
+    "Do you want to receive cash back on dining, including restaurants and cafes? If yes, indicate the desired cash back percentage.",
+    "Are you willing to pay an annual fee for a credit card? If so, what is the maximum annual fee you would consider?", 
+    "What is the ideal credit limit you would like on your credit card? This is the maximum amount you can borrow at any time.",
+    "Could you provide your current credit score range? This information helps in recommending a card that matches your credit profile.",
+]
+
+question_index = 0
+
+user_responses = {}
 
 @app.route("/")
 def index():
     return render_template('chat.html')
 
-
-# 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
+    global question_index
+    user_input = request.form["msg"]
 
+    if user_input.lower() in ['hi', 'hello', 'start']:
+        question_index = 0
+        user_responses.clear()
+        return "Hello! I am your credit card recommendation assistant. " + questions[question_index]
 
-def get_Chat_response(text):
+    if question_index < len(questions):
+        user_responses[questions[question_index]] = user_input
+        question_index += 1
+        if question_index < len(questions):
+            return questions[question_index]
+        else:
+            return process_responses_with_gpt()
 
-    # Let's chat for 5 lines
-    for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+    return "I'm not sure how to respond to that. Could you please rephrase?"
 
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+def process_responses_with_gpt():
+    prompt = "Process and categorize user responses based on the following criteria:\n\n"
+    prompt += "1. Travel Rewards (Cash Back):\n   - 0% : 0\n   - 1% : 1\n   - 2% : 2\n   - 3% : 3\n   - 4% : 4\n   - 5% : 5\n   - 6+% : 6\n\n"
+    prompt += "2. Entertainment Rewards (Cash Back):\n   - [Same breakdown as Travel Rewards]\n\n"
+    prompt += "3. Dining Rewards (Cash Back):\n   - [Same breakdown as Travel Rewards]\n\n"
+    prompt += "4. Annual Fee:\n   - $0: 0\n   - $1 - $100: 1\n   - $101 - $200: 2\n   - $201 - $300: 3\n   - $301 - $400: 4\n   - $401 - $500: 5\n   - $500+: 6\n\n"
+    prompt += "5. Credit Line:\n   - $0-$5,000: 1\n   - $5,001-$10,000: 2\n   - $10,001-$15,000: 3\n   - $15,001-$20,000: 4\n   - $20,001-$25,000: 5\n   - $25,001-$30,000: 6\n\n"
+    prompt += "6. Credit Score:\n   - 300 - 579: 0\n   - 580 - 669: 1\n   - 670 - 739: 2\n   - 740 - 799: 3\n   - 800 - 850: 4\n\n"
+    prompt += "Given the user responses:\n"
+    for q, response in zip(questions, user_responses.values()):
+        prompt += f"- {q}: {response}\n"
+    prompt += "\nCategorize each response according to the sections above and format the output as a string in the following format: 'Travel: X, Entertainment: Y, Dining: Z, Annual Fee: A, Credit Line: B, Credit Score: C'."
 
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    try:
+        response = client.completions.create(
+            model="gpt-3.5-turbo",
+            prompt=prompt,
+        )
 
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        if isinstance(response, str):
+            return response.strip()
+        else:
+            return response.choices[0].text.strip()
+
+    except Exception as e:
+        print(f"Error: {str(e)}")  
+        return "There was an error processing your credit card recommendation."
+
 
 
 if __name__ == '__main__':
